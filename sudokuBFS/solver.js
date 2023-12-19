7bc66726bccc476316b5ca159b102afaf406f66c
Andi Hyseni <andi.hyseni@foleja.com>
2023-12-20 00:45:02 +0100

    sudokuBFS update

--- sudokuBFS/solver.js
+++ sudokuBFS/solver.js
@@ -1,138 +1,113 @@
-function isSafe(board, row, col, num) {
-    for (let x = 0; x < 9; x++) {
-      if (board[row][x] === num || board[x][col] === num) {
-        return false;
-      }
-    }
-  
-    const startRow = Math.floor(row / 3) * 3;
-    const startCol = Math.floor(col / 3) * 3;
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (board[startRow + i][startCol + j] === num) {
-          return false;
-        }
-      }
-    }
-  
-    return true;
-  }
-  
-  function solveSudoku(board) {
-    for (let row = 0; row < 9; row++) {
-      for (let col = 0; col < 9; col++) {
-        if (board[row][col] === 0) {
-          for (let num = 1; num <= 9; num++) {
-            if (isSafe(board, row, col, num)) {
-              board[row][col] = num;
-  
-              if (solveSudoku(board)) {
-                return true;
-              }
-  
-              board[row][col] = 0;
-            }
-          }
-          return false;
-        }
-      }
-    }
-  
-    return true;
-  }
-  
-  function printSudoku(board) {
-    for (let i = 0; i < 9; i++) {
-      console.log(board[i].join(' '));
-    }
-    console.log('\n');
-  }
-  
-  function solveSudokuBFS(initialBoard) {
-    const queue = [initialBoard];
-  
-    while (queue.length > 0) {
+/**
+ * @param {character[][]} board
+ * @return {void}
+ */
+
+function solveSudoku(board) {
+  const n = board.length;
+  const queue = [];
+
+  queue.push(board);
+
+  while (queue.length > 0) {
       const currentBoard = queue.shift();
-  
-      let foundEmptyCell = false;
-      for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-          if (currentBoard[i][j] === 0) {
-            foundEmptyCell = true;
-            for (let num = 1; num <= 9; num++) {
-              if (isSafe(currentBoard, i, j, num)) {
-                const newBoard = JSON.parse(JSON.stringify(currentBoard));
-                newBoard[i][j] = num;
-                queue.push(newBoard);
-              }
-            }
-            break;
+      const emptyCell = findEmptyCell(currentBoard, n);
+
+      if (!emptyCell) {
+          console.log("Sudoku Solved:");
+          console.log(currentBoard.map(row => row.join(" ")).join("\n"));
+          return;
+      }
+
+      const [row, col] = emptyCell;
+
+      for (let i = 1; i <= 9; i++) {
+          const newVal = i.toString();
+          if (isValid(currentBoard, row, col, n, newVal)) {
+              const newBoard = copyBoard(currentBoard);
+              newBoard[row][col] = newVal;
+              queue.push(newBoard);
           }
-        }
-        if (foundEmptyCell) {
-          break;
-        }
       }
-  
-      if (!foundEmptyCell && solveSudoku(currentBoard)) {
-        return currentBoard;
-      }
-    }
-  
-    return null;
   }
-  
-  const easyLevelSudoku = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9],
-  ];
-  
-  const mediumLevelSudoku = [
-    [8, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 3, 6, 0, 0, 0, 0, 0],
-    [0, 7, 0, 0, 9, 0, 2, 0, 0],
-    [0, 5, 0, 0, 0, 7, 0, 0, 0],
-    [0, 0, 0, 0, 4, 5, 7, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 3, 0],
-    [0, 0, 1, 0, 0, 0, 0, 6, 8],
-    [0, 0, 8, 5, 0, 0, 0, 1, 0],
-    [0, 9, 0, 0, 0, 0, 4, 0, 0]
-  ];
-  
-  const hardLevelSudoku = [
-    [0, 0, 0, 0, 6, 0, 4, 0, 0],
-    [0, 7, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 7, 0, 0, 0, 0, 0, 0],
-    [0, 2, 5, 0, 0, 0, 0, 0, 0],
-    [6, 0, 0, 0, 5, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0]
-  ];
-  
-  console.log("Easy Level Sudoku:");
-  printSudoku(easyLevelSudoku);
-  console.log("Easy Level Sudoku solved:");
-  solveSudoku(easyLevelSudoku);
-  printSudoku(easyLevelSudoku);
-  
-  console.log("Medium Level Sudoku:");
-  printSudoku(mediumLevelSudoku);
-  console.log("Medium Level Sudoku solved:");
-  solveSudoku(mediumLevelSudoku);
-  printSudoku(mediumLevelSudoku);
-  
-  console.log("Hard Level Sudoku:");
-  printSudoku(hardLevelSudoku);
-  console.log("Hard Level Sudoku solved:");
-  solveSudoku(hardLevelSudoku);
-  printSudoku(hardLevelSudoku);
-  
+
+  console.log("No solution exists.");
+}
+
+function findEmptyCell(board, n) {
+  for (let row = 0; row < n; row++) {
+      for (let col = 0; col < n; col++) {
+          if (board[row][col] === ".") {
+              return [row, col];
+          }
+      }
+  }
+  return null;
+}
+
+function copyBoard(board) {
+  return board.map(row => [...row]);
+}
+
+function isValid(board, row, col, n, newVal) {
+  const boxRow = Math.floor(row / 3) * 3;
+  const boxCol = Math.floor(col / 3) * 3;
+
+  for (let i = 0; i < n; i++) {
+      if (board[row][i] === newVal || board[i][col] === newVal) {
+          return false;
+      }
+
+      const curRow = boxRow + Math.floor(i / 3);
+      const curCol = boxCol + Math.floor(i % 3);
+
+      if (board[curRow][curCol] === newVal) {
+          return false;
+      }
+  }
+
+  return true;
+}
+
+// easy, medium, hard
+const puzzles = [
+  [
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+  ],
+  [
+    ["7", "8", ".", ".", ".", ".", "5", ".", "6"],
+    [".", "6", "5", "8", ".", "4", ".", ".", "."],
+    [".", ".", ".", "6", ".", ".", ".", "8", "9"],
+    [".", ".", "7", "1", ".", ".", ".", ".", "."],
+    ["4", ".", "6", ".", ".", "9", ".", ".", "1"],
+    ["1", "2", ".", "3", ".", ".", "8", "6", "."],
+    [".", ".", ".", ".", "3", "1", "9", ".", "."],
+    [".", ".", ".", ".", ".", ".", "1", ".", "."],
+    ["3", "9", "1", ".", ".", ".", "6", "5", "4"]
+  ],
+  [
+    ["9", ".", ".", ".", ".", ".", "5", "3", "."],
+    ["2", ".", ".", "8", ".", "1", ".", ".", "9"],
+    [".", "3", ".", ".", ".", "9", ".", "2", "7"],
+    ["3", ".", ".", ".", ".", ".", ".", "6", "."],
+    [".", ".", "6", "9", ".", "8", ".", ".", "4"],
+    ["4", "9", ".", "7", ".", ".", ".", ".", "."],
+    [".", ".", "4", ".", ".", ".", ".", "1", "."],
+    [".", ".", ".", "1", ".", "7", ".", ".", "."],
+    ["1", ".", ".", ".", ".", ".", ".", ".", "."]
+  ]
+];
+
+puzzles.forEach((puzzle, index) => {
+  console.log(`Solving Puzzle ${index + 1}:`);
+  solveSudoku(puzzle);
+  console.log("\n");
+});
